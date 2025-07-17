f34cb18bd00989ed86ea381d6d0850e8f3440125
Priyanka875585 <shahapriyanka08@gmail.com>
2025-07-17 20:16:01 +0530

    Create assignment2.js

---
+++ Lab1/assignment2.js
@@ -0,0 +1,6 @@
+var a="Priyanka"; 
+var b="Shaha"; 
+var c="MCA-I";
+console.log("Length of first string = " + a.length);
+console.log("Length of second string = " + b.length); 
+console.log("Length of third string = " + c.length); 
